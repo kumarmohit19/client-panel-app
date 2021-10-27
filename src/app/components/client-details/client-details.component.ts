013ab89920b58232c8fbadf05fb17a97f15da152
Kumar <mohitnevatru@gmail.com>
2021-10-27 13:09:21 +0530

    client details component and getclient method

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from 'src/app/models/Client';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-client-details',
   templateUrl: './client-details.component.html',
-  styleUrls: ['./client-details.component.css']
+  styleUrls: ['./client-details.component.css'],
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance: boolean = false;
+  showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit(): void {
+    // get id from url
+    this.id = this.route.snapshot.params['id'];
+    // get client
+
+    this.clientService.getClient(this.id).subscribe((client) => {
+      if (client != null) {
+        if (client.balance && client.balance > 0) {
+          this.hasBalance = true;
+        }
+
+        this.client = client;
+      }
+    });
   }
 
+  onDeleteClick() {}
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -16,7 +16,7 @@ export class ClientService {
   clientsCollection: AngularFirestoreCollection<Client>;
   clientDoc: AngularFirestoreDocument<Client>;
   clients: Observable<Client[]>;
-  client: Observable<Client>;
+  client: Observable<Client | null>;
 
   constructor(private afs: AngularFirestore) {
     this.clientsCollection = this.afs.collection('clients', (ref) =>
@@ -41,5 +41,22 @@ export class ClientService {
 
   newClient(client: Client) {
     this.clientsCollection.add(client);
+  }
+
+  getClient(id: string): Observable<Client | null> {
+    this.clientDoc = this.afs.doc<Client>(`clients/${id}`);
+    this.client = this.clientDoc.snapshotChanges().pipe(
+      map((action: any) => {
+        if (action.payload.exists === false) {
+          return null;
+        } else {
+          const data = action.payload.data() as Client;
+          data.id = action.payload.id;
+          return data;
+        }
+      })
+    );
+
+    return this.client;
   }
 }
