487512a98954cf4691473f6ce995721d6ead942c
Kumar <mohitnevatru@gmail.com>
2021-10-27 22:27:40 +0530

    edit client component completed

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from 'src/app/services/client.service';
+import { Client } from 'src/app/models/Client';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-client',
   templateUrl: './edit-client.component.html',
-  styleUrls: ['./edit-client.component.css']
+  styleUrls: ['./edit-client.component.css'],
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+  };
+  disableBalanceOnEdit: boolean = true;
 
-  constructor() { }
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit(): void {
+    // get id from url
+    this.id = this.route.snapshot.params['id'];
+
+    // get client
+    this.clientService.getClient(this.id).subscribe((client) => {
+      if (client != null) {
+        this.client = client;
+      }
+    });
   }
 
+  onSubmit({ value, valid }: NgForm) {
+    if (!valid) {
+      this.flashMessage.show('Please fill out the form correctly', {
+        cssClass: 'alert-danger',
+        timeout: 4000,
+      });
+    } else {
+      // add id to client
+      value.id = this.id;
+      // update client
+      this.clientService.updateClient(value);
+      // show message
+      this.flashMessage.show('Client updated succesfully', {
+        cssClass: 'alert-success',
+        timeout: 4000,
+      });
+      this.router.navigate([`/client/${this.id}`]);
+    }
+  }
 }
