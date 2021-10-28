b714096f60e1381a88b1e5948ffe097621b4e8bd
Kumar <mohitnevatru@gmail.com>
2021-10-28 09:27:25 +0530

    auth gaurd added and applied on nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,19 +12,33 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientDetailsComponent } from './components/client-details/client-details.component';
 
+import { AuthGaurd } from './gaurds/auth.gaurd';
+
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: '', component: DashboardComponent, canActivate: [AuthGaurd] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'client/add', component: AddClientComponent },
-  { path: 'client/edit/:id', component: EditClientComponent },
-  { path: 'client/:id', component: ClientDetailsComponent },
-  { path: 'settings', component: SettingsComponent },
+  {
+    path: 'client/add',
+    component: AddClientComponent,
+    canActivate: [AuthGaurd],
+  },
+  {
+    path: 'client/edit/:id',
+    component: EditClientComponent,
+    canActivate: [AuthGaurd],
+  },
+  {
+    path: 'client/:id',
+    component: ClientDetailsComponent,
+    canActivate: [AuthGaurd],
+  },
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGaurd] },
   { path: '**', component: NotFoundComponent },
 ];
 @NgModule({
   exports: [RouterModule],
-  declarations: [],
+  providers: [AuthGaurd],
   imports: [RouterModule.forRoot(routes)],
 })
 export class AppRoutingModule {}
