9ffdece03f35e21dd6e29fab44cda3046edbfcac
Kumar <mohitnevatru@gmail.com>
2021-10-28 10:59:12 +0530

    settings component created and saved in localstorage

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Settings } from '../../models/Settings';
+import { SettingsService } from 'src/app/services/settings.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
+  settings: Settings;
 
-  constructor() { }
+  constructor(
+    private settingService: SettingsService,
+    private flashMessage: FlashMessagesService
+  ) {}
 
   ngOnInit(): void {
+    this.settings = this.settingService.getSettings();
   }
 
+  onSubmit() {
+    this.settingService.updateSettings(this.settings);
+    this.flashMessage.show('Your settings are updated and saved', {
+      cssClass: 'alert-success',
+      timeout: 4000,
+    });
+  }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -7,13 +7,23 @@ import { Settings } from '../models/Settings';
 export class SettingsService {
   settings: Settings = {
     allowRegistration: true,
-    disableBalanceOnAdd: true,
     disableBalanceOnEdit: true,
+    disableBalanceOnAdd: true,
   };
 
-  constructor() {}
+  constructor() {
+    if (localStorage.getItem('clientPanelSettings') != null) {
+      this.settings = JSON.parse(
+        localStorage.getItem('clientPanelSettings') ?? ''
+      );
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  updateSettings(settings: Settings) {
+    localStorage.setItem('clientPanelSettings', JSON.stringify(settings));
   }
 }
